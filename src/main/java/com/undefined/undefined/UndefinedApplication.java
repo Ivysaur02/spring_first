e312036acb54ad9e5f52b27df0faeb8e77120099
Max Khairulov <max.hairulov@mail.ru>
2023-06-26 16:20:41 +0400

    Creating an application skeleton
    Dependencies were added: Lombok, Spring Web, Spring Security, H2 Database, PostgreSQL Driver, Validation, Testcontainers, etc.

---
+++ src/main/java/com/undefined/undefined/UndefinedApplication.java
@@ -0,0 +1,13 @@
+package com.undefined.undefined;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndefinedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UndefinedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/undefined/undefined/UndefinedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undefined.undefined;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndefinedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
