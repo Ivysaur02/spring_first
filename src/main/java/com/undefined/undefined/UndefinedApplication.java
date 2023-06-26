72ff5b68addd74b03dddb1c5a675eb6001156be7
Max Khairulov <max.hairulov@mail.ru>
2023-06-26 23:06:11 +0400

    Fix small problem

--- src/main/java/com/undefined/undefined/UndefinedApplication.java
+++ src/main/java/com/undefined/undefined/UndefinedApplication.java
@@ -1,35 +1,13 @@
 package com.undefined.undefined;
 
-import com.undefined.undefined.FeignApi.ApiClient;
-import com.undefined.undefined.FeignApi.ApiResponse;
-import feign.Feign;
-import feign.gson.GsonDecoder;
-import java.util.Map;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class UndefinedApplication {
+
     public static void main(String[] args) {
-        ApiClient client = Feign.builder()
-                .decoder(new GsonDecoder()) // JSON декодер, чтобы работать с JSON
-                .target(ApiClient.class, "https://openexchangerates.org"); // ApiClient - то, что мы работаем с GET запросом, который мы описали в интерфейсе
-
-        String date = "2023-06-25";
-        String appId = "62702b9e6068461fbf4a38ae88b88547"; // Код Даниила (НЕ ТРОГАТЬ)
-        String base = "USD"; // Изначальная ВАЛЮТА для котировки
-        String symbols = "AUD,GBP,EUR,RUB"; // Курс
-
-        ApiResponse response = client.getExchangeRates(date, appId, base, symbols); // Вызов GET запроса. Результат записывается в объект типа ApiResponse
-
-        // Обработка полученного ответа. Объект response
-            System.out.println("Disclaimer: " + response.getDisclaimer());
-            System.out.println("License: " + response.getLicense());
-            System.out.println("Timestamp: " + response.getTimestamp());
-            System.out.println("Base: " + response.getBase());
-            System.out.println("Rates:");
-            for (Map.Entry<String, Double> entry : response.getRates().entrySet()) {
-                String currencyCode = entry.getKey();
-                double rate = entry.getValue();
-                System.out.println(currencyCode + ": " + rate);
-        }
+        SpringApplication.run(UndefinedApplication.class, args);
     }
+
 }
