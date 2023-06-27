ecdb0955224259b3484d6aa2fd54571da5fdb20e
RailSafiullin <rail.safiyllin@icloud.com>
2023-06-27 23:41:49 +0400

    Add tag for controller

--- src/main/java/com/undefined/undefined/controller/TestController.java
+++ src/main/java/com/undefined/undefined/controller/TestController.java
@@ -2,6 +2,7 @@ package com.undefined.undefined.controller;
 
 import com.undefined.undefined.feign.dto.DTOResponseCurrency;
 import com.undefined.undefined.service.CurrencyService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 //TODO Remove later
+@Tag(name="Название игорь контроллера", description="Описание контролера")
 public class TestController {
     private final CurrencyService currencyService;
 
