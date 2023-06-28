e321667d6689c08f7495be5fb15a3cadffeb15ae
pxtheticcat <serega.ivanovv2012@gmail.com>
2023-06-28 16:54:01 +0400

    The first design approach was implemented
    Wow..

--- src/main/java/com/undefined/undefined/controller/TestController.java
+++ src/main/java/com/undefined/undefined/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.undefined.undefined.controller;
 
+import com.undefined.undefined.abstraction.api.DecodeApi;
 import com.undefined.undefined.feign.dto.DTOData;
 import com.undefined.undefined.feign.dto.DTOResponseCurrency;
 import com.undefined.undefined.service.AppService;
@@ -10,15 +11,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 //TODO Remove later
 @Tag(name="Тесты", description="Я не придумал")
-public class TestController {
+public class TestController implements DecodeApi {
     private final CurrencyService currencyService;
     private final GiphyService giphyService;
     private final AppService appService;
@@ -31,6 +35,12 @@ public class TestController {
     public ResponseEntity<DTOData> test1() {
         return ResponseEntity.ok(giphyService.getGiphy());
     }
+
     @GetMapping("/mainAppTest")
-    public ResponseEntity<String> appLogicTest() {return ResponseEntity.ok(appService.getResult());}
+    public ResponseEntity<String> appLogicTest() { return ResponseEntity.ok(appService.getResult()); }
+
+    @Override
+    public ResponseEntity<Void> decodeFile(MultipartFile file) {
+        return DecodeApi.super.decodeFile(file);
+    }
 }
