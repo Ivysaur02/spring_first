bf1fd908abc86debbaaf5eaaf01d0950f3266af2
Max Khairulov <max.hairulov@mail.ru>
2023-06-28 12:55:01 +0400

    Add service giphy

--- src/main/java/com/undefined/undefined/controller/TestController.java
+++ src/main/java/com/undefined/undefined/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.undefined.undefined.controller;
 
+import com.undefined.undefined.feign.dto.DTOData;
 import com.undefined.undefined.feign.dto.DTOResponseCurrency;
 import com.undefined.undefined.service.CurrencyService;
+import com.undefined.undefined.service.GiphyService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name="Название игорь контроллера", description="Описание контролера")
 public class TestController {
     private final CurrencyService currencyService;
+    private final GiphyService giphyService;
 
     @GetMapping("/test")
     public ResponseEntity<DTOResponseCurrency> test() {
         return ResponseEntity.ok(currencyService.getExchangeRate());
+    }
+
+    @GetMapping("/test1")
+    public ResponseEntity<DTOData> test1() {
+        return ResponseEntity.ok(giphyService.getGiphy());
     }
 }
