a4d30a26eac2908e0b100c3abccead29ab70ca5d
Ivysaur02 <ig.sochnev@mail.ru>
2023-06-28 20:36:20 +0400

    not working

---
+++ src/test/java/servicetest/AbstractTest.java
@@ -0,0 +1,36 @@
+package servicetest;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("servicetest")
+@AutoConfigureMockMvc
+public abstract class AbstractTest {
+
+//    protected static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(
+//            DockerImageName.parse("postgres:9.6")
+//                    .asCompatibleSubstituteFor("postgres"))
+//            .withReuse(true);
+//
+//    @DynamicPropertySource
+//    static void postgresProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+//        registry.add("spring.datasource.username", POSTGRES::getUsername);
+//        registry.add("spring.datasource.password", POSTGRES::getPassword);
+//    }
+//
+//    @BeforeAll
+//    public static void beforeAll() {
+//        POSTGRES.start();
+//    }
+}
+
